1204bb5a426a7597919fdabc8915e45a2e084f8c
Vangala Veera Reddy <v.veerareddy2023@gmail.com>
2026-07-10 19:59:11 +0530

    Completed premium landing page

---
+++ core/static/js/script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+
+    document.querySelector(".hero-left").classList.add("show");
+
+    document.querySelector(".hero-right").classList.add("show");
+
+});
